aa2f74ab1733181de3771248da63542cff0cd6c8
Nicholas Noly C. dela Torre <88265052+Nicktacku@users.noreply.github.com>
2023-11-17 19:13:54 +0800

    added all lexeme types

--- Tokens.py
+++ Tokens.py
@@ -6,7 +6,7 @@ class Token:
 class Digit(Token):
     def __init__(self, value):
         super().__init__(value, "DIGIT")
-    
+
     def __repr__(self):
         return self.token
 
@@ -18,9 +18,30 @@ class Float(Token):
         return self.token
 
 class Operator(Token):
-    def __init__(self, value):
-        super().__init__(value, "Operator")
+    def __init__(self, value, token):
+        super().__init__(value, token)
     
+    def __repr__(self):
+        return self.token
+
+class Delimeter(Token):
+    def __init__(self, value):
+        super().__init__(value, "Delimeter")
+
+    def __repr__(self):
+        return self.token
+
+class SpecialChar(Token):
+    def __init__(self, value, token):
+        super().__init__(value, token)
+
+    def __repr__(self):
+        return self.token
+
+class Lexeme(Token):
+    def __init__(self, value, token):
+        super().__init__(value, token)
+
     def __repr__(self):
         return self.token
 
--- lexer.py
+++ lexer.py
@@ -1,5 +1,5 @@
-from constants import digits, operations
-from Tokens import Digit, Float, Operator, Invalid
+from constants import *
+from Tokens import *
 
 class Lexer:
     def __init__(self, inp):
@@ -10,11 +10,17 @@ class Lexer:
 
     def to_token(self):
         while self.index < len(self.inp):
-            if self.current == " " or self.current == "\n":
+            not_comment = self.inp[self.index + 1] != "/" if len(self.inp) > self.index + 1 else None
+            is_multiplication = self.inp[self.index + 1] == " " if (len(self.inp) > self.index + 1) and self.current == "*" else True
+
+            if self.current in spaces:
+
                 self.move()
 
-            if self.current in digits:
+            if self.current == None:
+                break
 
+            if self.current in digits:
                 output = self.tokenize_digit()
                 if output == None:
                     return Invalid(self.inp)
@@ -23,12 +29,31 @@ class Lexer:
                     self.tokens.append(Float(output))
                 else:
                     self.tokens.append(Digit(output))
-            elif self.current in operations:
+            elif (self.current in operators or self.current in ["&", "|"]) and not_comment and is_multiplication:
+                print(self.inp[self.index + 1])
                 output = self.tokenize_operation()
                 if output == None:
                     return Invalid(self.inp)
                     break
-                self.tokens.append(Operator(output))
+                self.tokens.append(Operator(output[0], output[1]))
+            elif self.current in delimeters:
+                output = self.tokenize_delimeter()
+                if output == None:
+                    return Invalid(self.inp)
+                    break
+                self.tokens.append(Delimeter(output))
+            elif self.current in alphabet:
+                output = self.tokenize_lexeme()
+                if output == None:
+                    return Invalid(self.inp)
+                    break
+                self.tokens.append(Lexeme(output[0], output[1]))
+            elif self.current in special_characters:
+                output = self.tokenize_special_characters()
+                if output == None:
+                    return Invalid(self.inp)
+                    break
+                self.tokens.append(SpecialChar(output[0], output[1]))
         return self.tokens
 
     def move(self):
@@ -40,12 +65,16 @@ class Lexer:
 
     def tokenize_digit(self):
         numbers = ""
-        while self.current != " " and self.current != None and self.current != "\n":
+        while self.current not in spaces and self.current != None and self.current not in delimeters and self.current not in special_characters:
             if self.current not in digits and self.current != ".":
                 return None
 
             numbers += self.current
             self.move()
+
+        if numbers.count(".") > 1:
+            return None
+        
         return numbers
 
     def tokenize_operation(self):
@@ -53,8 +82,118 @@ class Lexer:
         while self.current != " " and self.current != None:
             operator += self.current
             self.move()
-        return operator
+        return (operator, operators[operator])
+
+    def tokenize_delimeter(self):
+        delimeter = delimeters[self.current]
+        self.move()
+        return delimeter
+
+    def tokenize_special_characters(self):
+        special_character = ""
+        quotes = ["'", '"']
+        if self.current in quotes:
+            value = ""
+            special_character = self.current
+            while self.current != " " and self.current != None and (self.current in quotes or self.current in alphabet):
+                value += self.current
+                self.move()
+            if value.endswith(("'", '"')):
+                return (value, "String")
+            else:
+                return None
+        elif self.current in comments:
+            value = ""
+            if self.current == "/":
+                while self.current != "\n" and self.current != None:
+                    value += self.current
+                    self.move()
+                if value.startswith("//"):
+                    return (value, "Comment")
+                else:
+                    return None
+            elif self.current == "#":
+                value += self.current
+                self.move()
+                while self.current != "#" and self.current != None:
+                    value += self.current
+                    self.move()
+                value += self.current
+                self.move()
+                if value.startswith("#") and value.endswith("#"):
+                    return (value, "MULTILINECOMMENT")
+                else:
+                    return None
+        elif self.current == "*":
+            value = ""
+            while self.current not in spaces and self.current != None:
+                value += self.current
+                self.move()
+            
+            if value in atomic_number:
+                return (value, "ATOMICNUMBERLITERAL")
+            elif value in atomic_symbol:
+                return (value, "ATOMICSYMBOLLITERAL")
+            elif value in positive_charge:
+                return (value, "POSITIVECHARGELITERAL")
+            elif value in negative_charge:
+                return (value, "NEGATIVECHARGELITERAL")
+            elif value in boyle:
+                return (value, "BOYLELITERAL")
+            elif value in charles:
+                return (value, "CHARLESLITERAL")
+            elif value in avogadro:
+                return (value, "AVOGADROLITERAL")
+            elif value in ideal_gas:
+                return (value, "IDEALGASLITERAL")
+            elif value in combined_gas:
+                return (value, "COMBINEDGASLITERAL")
+            else:
+                return None
+        else:
+            special_character += self.current
+            self.move()
+        return (special_character, special_characters[special_character])
+
+    def tokenize_lexeme(self):
+        lexeme = ""
+        identifier_valid = True
+
+        while self.current not in spaces and self.current != None and self.current not in special_characters:
+            
+            lexeme_accepted = self.current in digits or self.current in alphabet or self.current == "_"
+            if not lexeme_accepted:
+                identifier_valid = False
+            lexeme += self.current
+            self.move()
+
+        if lexeme in keywords:
+            return (lexeme, "Keyword")
+        elif lexeme in atomic_name:
+            return (lexeme, "Atomic_Name")
+        elif lexeme in molecular_formula:
+            return (lexeme, "Molecular_Formula")
+        elif lexeme in booleans:
+            return (lexeme, "BOOLEANLITERAL")
+
+        if len(self.inp) > self.index + 1:
+            if self.inp[self.index + 1] in alphabet:
+                lexeme += self.current
+                self.move()
+                while self.current not in spaces and self.current != None:
+                    print(self.current)
+                    lexeme += self.current
+                    self.move()
+                if lexeme in compound_name:
+                    return (lexeme, "Compound_Name")
+            elif identifier_valid:
+                return (lexeme, "Identifier")
+        elif identifier_valid:
+            return (lexeme, "Identifier")
+        
+        print("here")
+
 
 if __name__ == "__main__":
-    lexer = Lexer("1.2 * 1")
+    lexer = Lexer('num1 = 2;')
     print(lexer.to_token())
