121885af7c17fa5bba14dccb475b17bcdc3fb723
Nicholas Noly C. dela Torre <88265052+Nicktacku@users.noreply.github.com>
2024-01-28 20:24:57 +0800

    debugged

--- lexer.py
+++ lexer.py
@@ -12,19 +12,23 @@ class Lexer:
 
     def to_token(self):
         number_appeared = False
+        is_string = False
 
         while self.index < len(self.inp) and self.current != None:
+            print("starting ", self.current)
             # skips out spaces
             if self.current in spaces:
+                print("space moved ", self.current)
                 self.move()
-                
                 if self.current is None:
+                    print("in none ", self.current)
                     break
 
             # defines if its a comment or divide
             not_comment = self.inp[self.index + 1] != "/" if len(self.inp) > self.index + 1 else True
 
-            if (self.current == "-" or self.current == "+") and not number_appeared:
+            if (self.current == "-" or self.current == "+") and number_appeared:
+                print("detected digit 1", self.current)
                 number_appeared = True
                 output = self.tokenize_digit()
                 if "INVALID" in output:
@@ -33,53 +37,101 @@ class Lexer:
                     self.tokens.append(Float(output))
                 else:
                     self.tokens.append(Digit(output))
+                
             elif self.current in digits:
+                print("detected digit 2 ", self.current)
                 number_appeared = True
                 output = self.tokenize_digit()
                 if "INVALID" in output:
                     self.tokens.append(Invalid(output[0]))
-                if "." in output:
+                elif "." in output:
                     self.tokens.append(Float(output))
                 else:
                     self.tokens.append(Digit(output))
+                    
             elif (self.current in operators or self.current in ["&", "|"]) and not_comment:
+                print("operators ", self.current)
                 number_appeared = False
 
                 output = self.tokenize_operation()
 
                 if "INVALID" in output:
                     self.tokens.append(Invalid(output[0]))
-                self.tokens.append(Operator(output[0], output[1]))
+                else:
+                    self.tokens.append(Operator(output[0], output[1]))
+                
             elif self.current in delimeters:
+                print("delimeters ", self.current)
                 output = self.tokenize_delimeter()
                 if "INVALID" in output:
                     self.tokens.append(Invalid(output[0]))
-                self.tokens.append(Delimeter(output))
+                else:
+                    self.tokens.append(Delimeter(output))
+                
             elif self.current in alphabet:
-                output = self.tokenize_lexeme()
+                print("alphabet ", self.current)
+
+                output = self.tokenize_lexeme(is_string)
+
+                    
                 if "INVALID" in output:
                     self.tokens.append(Invalid(output[0]))
-                self.tokens.append(Lexeme(output[0], output[1]))
+                else:
+                    self.tokens.append(Lexeme(output[0], output[1]))
+                
             elif self.current in special_characters:
-                if self.current == "_":
+                quotations = ["'", '"']
+                print("special characters ", self.current)
+                if self.current in quotations:
+                    output = self.tokenize_string()
+                    if "INVALID" in output:
+                        self.tokens.append(Invalid(output[0]))
+                    else:
+                        self.tokens.append(SpecialChar(output[0], output[1]))
+
+                    output = self.tokenize_string()
+                    if "INVALID" in output:
+                        self.tokens.append(Invalid(output[0]))
+                    else:
+                        self.tokens.append(Lexeme(output[0], output[1]))
+
+                    output = self.tokenize_string()
+                    if "INVALID" in output:
+                        self.tokens.append(Invalid(output[0]))
+                    else:
+                        self.tokens.append(SpecialChar(output[0], output[1]))
+                    continue
+                elif self.current == "_":
                     output = self.tokenize_lexeme()
                 else:
                     output = self.tokenize_special_characters()
+                    
                 if "INVALID" in output:
                     self.tokens.append(Invalid(output[0]))
-                self.tokens.append(SpecialChar(output[0], output[1]))
+                else:
+                    print(self.current)
+                    self.tokens.append(SpecialChar(output[0], output[1]))
+                
             elif self.current == ".":
-                    output = self.tokenize_digit()
-                    if output == None:
-                        return Invalid(self.current)
+                print("float", self.current)
+                output = self.tokenize_digit()
+                if output == None:
+                    return Invalid(self.current)
+                else:
                     self.tokens.append(Float(output))
+                
             else:
+                print("else checker ", self.current)
                 if self.current in spaces:
                     self.move()
                 else:
-                    invalid = self.current
+                    invalid = ""
+                    
+                    while self.current not in spaces and self.current is not None:
+                        invalid += self.current
+                        self.move()
                     self.tokens.append(Invalid(invalid))
-                    self.move()
+                    
 
                 if self.current is None:
                     break
@@ -100,15 +152,19 @@ class Lexer:
         not_digit = False
 
         while self.current not in spaces and self.current != None and self.current in valid_digits:
-            if self.current not in valid_digits:
-                not_digit = True
-
             numbers += self.current
             self.move()
+            if self.current not in valid_digits:
+                not_digit = True
+                while self.current not in spaces and self.current != None:
+                    numbers += self.current
+                    self.move()
+
+                
 
 
         if not_digit:
-            return (numbers)
+            return (numbers, "INVALID")
 
 
         if numbers.count(".") > 1:
@@ -201,7 +257,7 @@ class Lexer:
         return (special_character, special_characters[special_character])
 
 
-    def tokenize_lexeme(self):
+    def tokenize_lexeme(self, is_string):
         lexeme = ""
         valid_identifier = alphabet + "_" + digits
         identifier_valid = True
@@ -215,8 +271,9 @@ class Lexer:
             lexeme += self.current
 
             self.move()
-
-        if lexeme in keywords:
+        if is_string:
+            return(lexeme, "STRING")
+        elif lexeme in keywords:
             return (lexeme, "KEYWORD")
         elif lexeme in booleans:
             return (lexeme, "BOOLEANLITERAL")
@@ -236,6 +293,21 @@ class Lexer:
             return (lexeme, "CONSTANT")
         elif identifier_valid:
             return (lexeme, "IDENTIFIER")
+        
+    def tokenize_string(self):
+        print("entered")
+        value = ""
+        quotations = ["'", '"']
+        
+        if self.current in quotations:
+            value += self.current
+            self.move()
+            return (value, special_characters[value])
+        else:
+            while self.current != None and self.current not in quotations:
+                value += self.current
+                self.move()
+            return (value, "STRING")
 
 
 
